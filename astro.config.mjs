dd20187be2457cc964bb077142d4c4eb5f1bc1c5
Sebastian Araque <cbastian97araque@gmail.com>
2026-04-25 11:34:46 -0500

    ## Automatización con Github Actions
    1. Creacón de carpeta /dist con el proyecto para depsliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tuto-by-cbastian-araque.netlify.app/"
+  site: "https://cbastian-araque.github.io", // Ej: GH Pages, Netlify, Heroku, etc...
+  base: "/astro-blog" // Carpeta de alojamiento
 });
