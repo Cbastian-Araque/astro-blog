a82f543220522587326af3e1763ad0fa30cfdc51
Sebastian Araque <cbastian97araque@gmail.com>
2026-04-25 11:54:04 -0500

    ## Automatización con Github Actions
    1. Solución: Actualización de rutas de URL en el componenete de navegación

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://cbastian-araque.github.io", // Ej: GH Pages, Netlify, Heroku, etc...
-  base: "/astro-blog" // Carpeta de alojamiento
+  base: "/astro-blog/" // Carpeta de alojamiento
 });
